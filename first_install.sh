4e59269880dbfcef4c9dba54e9f6ca9cca7e7c9a
Nicolas Béreux <nicolas.bereux@gmail.com>
2021-04-14 15:00:28 +0200

    initial commit

---
+++ first_install.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+sudo apt install build-essential git texlive-full r-base r-base-dev emacs 
---
+++ install_dependencies.sh
@@ -0,0 +1,30 @@
+#! /bin/sh
+sudo apt install polybar libgl1-mesa-dev libpulse0 libpulse-dev libxext6 libxext-dev libxrender-dev libxcomposite-dev liblua5.3-dev liblua5.3 lua-lgi lua-filesystem libobs0 libobs-dev meson build-essential gcc libxcb-xinerama0-dev libxcb-icccm4-dev libxcb-randr0-dev libxcb-util0-dev libxcb-ewmh-dev libxcb-keysyms1-dev libxcb-shape0-dev i3 zsh
+sudo ldconfig
+
+#installation des fonts
+chmod -R 644 fonts
+
+#installation de bspwm
+git clone https://github.com/baskerville/bspwm.git
+git clone https://github.com/baskerville/sxhkd.git
+cd bspwm && make && sudo make install
+cd ../sxhkd && make && sudo make install
+
+#installation de glava
+cd glava
+meson build --prefix /usr
+ninja -C build
+sudo ninja -C build install
+
+#installation de oh my zsh + powerlevel10k
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+
+
+#copie des fichiers de configuration
+mkdir -p ~/.config/{bspwm,sxhkd}
+cp /usr/local/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/
+cp /usr/local/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/
+chmod u+x ~/.config/bspwm/bspwmrc
